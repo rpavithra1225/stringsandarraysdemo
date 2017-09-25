27af0bdd0dfd0dbc847509cb0c7e929ca34c95ec
Pavithra Rajan <pr384@njit.edu>
2017-09-25 18:46:15 -0400

    Added 1st string func

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
   $obj = new main();
-  $title = "This is to demonstrate String and Array functions";
-  $obj->printthis($title);
+  $text = "This is to demonstrate string functions";
+  $obj->stringfunctionsdemo($text);
   
   $array = array(1,2,3,4,5,6,7);
-  $obj->printArray($array);
+  //$obj->printArray($array);
 
   class main {
 
@@ -15,10 +15,26 @@
 
     }
 
+    public function printfunction($text) {
+      echo '<h1>print function demo </h1>';
+      print($text);
+      echo '<hr>';
+    }
+
+    public function stringfunctionsdemo($text) {
+      $this->printfunction($text);
+    }
+
     
+
+    public function arrayfunctionsdemo($array) {
+      echo '<h1>array print function</h1>';
+      print_r($array);
+      echo '<hr>';
+    }
     public function __destruct() {
 
-      echo '</br> That\'s it! Thanks!;
+      echo '</br> That\'s it! Thanks!';
 
     }
 
