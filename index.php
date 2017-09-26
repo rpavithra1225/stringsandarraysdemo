a6dc81130fb2ef8bb678739aa740e5ad96d972e4
Pavithra Rajan <pr384@njit.edu>
2017-09-26 14:31:22 -0400

    Formatting

--- index.php
+++ index.php
@@ -30,13 +30,13 @@
     }
 
     public function printstrfunc($text) {
-      echo '<h1>print function demo </h1>';
+      echo '<h1>1. print function demo </h1>';
       print($text);
       echo '<hr>';
     }
 
     public function strfunc_explode($text) {
-      echo '<h1>explode function demo </h1>';
+      echo '<h1>2. explode function demo </h1>';
       $pieces = explode(" ", $text);
       echo $pieces[0];
       echo $pieces[1];
@@ -44,14 +44,14 @@
     }
 
     public function strfunc_similar($text) {
-      echo '<h1>similar_text function demo </h1>';
+      echo '<h1>3. similar_text function demo </h1>';
       $percent = similar_text(" ", $text, $percent);
       echo $percent;
       echo '<hr>';
     }
 
     public function strfunc_strreplace($text) {
-      echo '<h1>str_replace function demo </h1>';
+      echo '<h1>4. str_replace function demo </h1>';
       $vowels = array("a", "e", "i", "o", "u", "A", "E", "I", "O", "U");
       $replacedstr = str_replace($vowels, " ", $text);
       echo $replacedstr;
@@ -59,37 +59,37 @@
     }
 
     public function strfunc_strrepeat() {
-      echo '<h1>str_repeat function demo </h1>';
+      echo '<h1>5. str_repeat function demo </h1>';
       echo str_repeat("Repeat", 10);
       echo '<hr>';
     }
 
     public function strfunc_strpad() {
-      echo '<h1>str_pad function demo </h1>';
+      echo '<h1>6. str_pad function demo </h1>';
       echo str_pad("Welcome", 10, "_", STR_PAD_BOTH);
       echo '<hr>';
     }
 
     public function strfunc_strreverse($text) {
-      echo '<h1>strrev function demo </h1>';
+      echo '<h1>7. strrev function demo </h1>';
       echo strrev($text);
       echo '<hr>';
     }
 
     public function strfunc_substr($text) {
-      echo '<h1>substr function demo </h1>';
+      echo '<h1>8. substr function demo </h1>';
       echo substr($text, -4, 3);
       echo '<hr>';
     }
 
     public function strfunc_substrcount($text) {
-      echo '<h1>substr_count function demo </h1>';
+      echo '<h1>9. substr_count function demo </h1>';
       echo substr_count($text, 'is', 3);
       echo '<hr>';
     }
 
     public function strfunc_strpos($text) {
-      echo '<h1>strpos function demo </h1>';
+      echo '<h1>10. strpos function demo </h1>';
       echo strpos($text, 's', 4);
       echo '<hr>';
     }
@@ -109,32 +109,32 @@
     }
 
     public function arrfunc_arraycount($array) {
-      echo '<h1>array_count_values function demo</h1>';
+      echo '<h1>1. array_count_values function demo</h1>';
       print_r(array_count_values($array));
       echo '<hr>';
     }
 
     public function arrfunc_arrayunique($array) {
-      echo '<h1>array_unique function demo</h1>';
+      echo '<h1>2. array_unique function demo</h1>';
       print_r(array_unique($array));
       echo '<hr>';
     }
 
     public function arrfunc_arraycombine($array) {
       $arraydummy = array('11','22','33','44','55');
-      echo '<h1>array_combine function demo</h1>';
+      echo '<h1>3. array_combine function demo</h1>';
       print_r(array_combine($arraydummy, $array));
       echo '<hr>';
     }
 
     public function arrfunc_arraychunk($array) {
-      echo '<h1>array_chunk function demo</h1>';
+      echo '<h1>4. array_chunk function demo</h1>';
       print_r(array_chunk($array, 2, true));
       echo '<hr>';
     }
 
     public function arrfunc_arrayslice($array) {
-      echo '<h1>array_slice function demo</h1>';
+      echo '<h1>5. array_slice function demo</h1>';
       print_r(array_slice($array,-3, -1, true));
       echo '<hr>';
     }
@@ -142,25 +142,25 @@
 
     public function arrfunc_arraydiff($array) {
       $local = array(1,2,3,4,5);
-      echo '<h1>array_diff function demo</h1>';
+      echo '<h1>6. array_diff function demo</h1>';
       print_r(array_diff($array,$local));
       echo '<hr>';
     }
 
     public function arrfunc_arrayfill($array) {
-      echo '<h1>array_fill function demo</h1>';
+      echo '<h1>7. array_fill function demo</h1>';
       print_r(array_fill(-5, 5, 'Hello<br/>'));
       echo '<hr>';
     }
 
     public function arrfunc_arrayflip($array) {
-      echo '<h1>array_flip function demo</h1>';
+      echo '<h1>8. array_flip function demo</h1>';
       print_r(array_flip($array));
       echo '<hr>';
     }
 
      public function arrfunc_arraykeyexists() {
-      echo '<h1>array_key_exists function demo</h1>';
+      echo '<h1>9. array_key_exists function demo</h1>';
 
       $search_array = array('first' => 1, 'second' => 4);
 
@@ -171,7 +171,7 @@
     }
 
     public function arrfunc_rsort($array) {
-      echo '<h1>array_rsort function demo</h1>';
+      echo '<h1>10. array_rsort function demo</h1>';
       
       rsort($array);
       foreach ($array as $key => $val) {
